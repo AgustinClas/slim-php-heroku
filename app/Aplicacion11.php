16725eeb4c1288c255a87d0718cb52c8c302acf4
Agustin Clas <agustinclasst@gmail.com>
2021-03-23 19:30:55 -0300

    commit aplicacion 11

---
+++ app/Aplicacion11.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+Agustin Clas
+Aplicación No 11 (Potencias de números)
+Mostrar por pantalla las primeras 4 potencias de los números del uno 1 al 4 (hacer una función
+que las calcule invocando la función pow).
+*/
+
+    for($i=1;$i<5;$i++){
+        echo pow($i,$i);
+    }
+
+?>
