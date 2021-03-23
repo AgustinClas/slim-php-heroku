54c6fc612c214450d396d6629925e32ac14257d9
Agustin Clas <agustinclasst@gmail.com>
2021-03-23 20:58:52 -0300

    Aplicacion13

---
+++ app/Aplicacion13.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+Agustin Clas
+Aplicación No 13 (Invertir palabra)
+Crear una función que reciba como parámetro un string ($palabra) y un entero ($max). La
+función validará que la cantidad de caracteres que tiene $palabra no supere a $max y además
+deberá determinar si ese valor se encuentra dentro del siguiente listado de palabras válidas:
+“Recuperatorio”, “Parcial” y “Programacion”. Los valores de retorno serán:
+1 si la palabra pertenece a algún elemento del listado.
+0 en caso contrario.
+*/
+
+    function validarPalabras($palabra, $max){
+        $retorno = 0;
+
+        if(strlen($palabra) < $max){
+            if(strcmp($palabra, "Recuperatorio")==0 || strcmp($palabra, "Parcial")==0 || strcmp($palabra, "Programacion") == 0){
+                $retorno = 1;
+            } 
+        }
+        return $retorno;
+    }
+
+    validarPalabras("casa", 6);
+?>
