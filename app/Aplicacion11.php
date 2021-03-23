692e43c16e1152195602dbc305b0f728b22e73f1
Agustin Clas <agustinclasst@gmail.com>
2021-03-23 20:07:02 -0300

    aplicacion11 C

--- app/Aplicacion11.php
+++ app/Aplicacion11.php
@@ -7,8 +7,22 @@ Mostrar por pantalla las primeras 4 potencias de los números del uno 1 al 4 (ha
 que las calcule invocando la función pow).
 */
 
+function calcularCuadrados(){
+
     for($i=1;$i<5;$i++){
-        echo pow($i,$i);
+        echo "Potencias de $i";
+        echo "<br>";
+
+     for($j=1;$j<5;$j++){
+            echo pow($i,$j);
+            echo "<br>";
+        }
+
+        echo "<br>";
+
+
     }
+}
+    calcularCuadrados();
 
 ?>
